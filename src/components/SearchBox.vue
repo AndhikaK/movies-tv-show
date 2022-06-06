9e38755c90351f22758d4731423f6a58906ba83b
AndhikaK <andhika.smandading@gmail.com>
2022-06-06 20:52:32 +0700

    complete search component

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -3,18 +3,21 @@
     <div class="search-box bg-slate-300 rounded-xl place-self-center text-slate-900">
       <div class="p-3 grid grid-cols-[auto_1fr_auto] place-items-center">
         <i class='bx bx-search-alt-2 text-3xl'></i>
-        <input type="text" class="p-2 w-full bg-transparent focus:outline-none text-xl" placeholder="Search movies or tv shows...">
+        <input type="text" v-on:keyup.enter="handleInput" v-model="store.search.query" class="p-2 w-full bg-transparent focus:outline-none text-xl" placeholder="Search movies or tv shows...">
         <i @click="store.toggleSearch()" class='bx bx-x ml-2 text-3xl text-slate-400 hover:text-slate-900 rounded-full'></i>
       </div>
       <div class="max-h-[400px] grid border-t border-t-slate-400 overflow-y-auto">
         <div v-if="getHistoryLength() > 0">
           <div class="p-4 px-5 font-bold text-lg">Recent</div>
           <div class="mb-5">
-            <div v-for="item in history" class="px-5 py-3 grid grid-cols-[1fr_auto] content-center border-t border-t-slate-400">
-              <div>Hasil search</div>
-              <i @click="store.toggleSearch()" class='bx bx-x ml-2 text-3xl text-slate-400 hover:text-slate-900 rounded-full'></i>
+            <div v-for="item in history.slice().reverse()" class="px-5 py-3 grid grid-cols-[1fr_auto] content-center border-t border-t-slate-400">
+              <div>{{item }}</div>
+              <i @click="removeHistory(item)" class='bx bx-x ml-2 text-3xl text-slate-400 hover:text-slate-900 rounded-full'></i>
             </div>
           </div>
+        </div>
+        <div v-else>
+          <div class="py-10 px-5 text-center">No recent searches</div>
         </div>
       </div>
     </div>
@@ -35,6 +38,21 @@ export default {
   methods: {
     getHistoryLength() {
       return this.store.search.history.length;
+    },
+    handleInput() {
+      if (
+        !this.history.includes(store.search.query) &&
+        this.store.search.query !== ""
+      ) {
+        this.store.toggleSearch();
+        this.store.addHistory(this.store.search.query);
+      }
+      this.store.emptySearch();
+    },
+    reverseHistory() {},
+    removeHistory(history) {
+      //   console.log(history);
+      this.store.removeHistory(history);
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -5,9 +5,24 @@ export const store = reactive({
   count: 2,
   search: {
     isShow: false,
-    history: ["Terluka", "Terluka", "Terluka", "Terluka"],
+    query: "",
+    history: [],
   },
   toggleSearch() {
     this.search.isShow = !this.search.isShow;
+  },
+  emptySearch() {
+    this.search.query = "";
+  },
+  addHistory(q) {
+    this.search.history.push(q);
+  },
+  removeHistory(history) {
+    const index = this.search.history.indexOf(history);
+
+    if (index > -1) {
+      this.search.history.splice(index, 1);
+    }
+    console.log(this.search.history);
   },
 });
